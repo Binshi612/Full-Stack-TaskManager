7d835fd43521c74343a8662d5fad2d083ad58f5e
Binshi <binshiferoke@gmail.com>
2022-05-19 21:38:48 +0530

    Edit page created

--- client/src/Pages/EditTask.jsx
+++ client/src/Pages/EditTask.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import "./EditTask.css";
 
 function EditTask() {
-  return <div>EditTask</div>;
+  return (
+    <div className="edittask">
+      <h1>Edit Task</h1>
+      <div className="contents">
+        <form className="form" action="">
+          <div className="item">
+            <label>Task Name :</label>
+            <br />
+            <input className="input" type="text" />
+          </div>
+          <div className="item">
+            <label>Description :</label> <br />
+            <input className="input" type="text" />
+          </div>
+          <div className="item">
+            <label>Due Date :</label> <br />
+            <input className="input" type="date" />
+          </div>
+          <div className="item">
+            <label>Difficulty :</label> <br />
+            <div>
+              <input className="radio" type="radio" value="Easy" />
+              Easy
+            </div>
+            <div>
+              <input className="radio" type="radio" value="Medium" />
+              Medium
+            </div>
+            <div>
+              <input className="radio" type="radio" value="Difficult" />
+              Difficult
+            </div>
+          </div>
+          <div className="footer">
+            <button className="btn">Updated</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default EditTask;
